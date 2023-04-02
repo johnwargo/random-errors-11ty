2bf79dd4244dd21c8e0c974cf2513d298faa11a0
John M. Wargo <johnwargo@users.noreply.github.com>
2023-04-02 05:54:07 -0400

    Update pub.cmd

--- pub.cmd
+++ pub.cmd
@@ -2,7 +2,7 @@ git add -A
 git commit -m "updated %date% %time%"
 git push
 call npm version patch
+call gen-build-info src/_data
 git add -A
 git commit -m "Updated package version"
 git push
-call gen-build-info src/_data
