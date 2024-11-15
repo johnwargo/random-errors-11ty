9d00e9768c62489053e57ce96bcc007467487fdf
johnwargo <john@johnwargo.com>
2024-11-15 07:25:53 -0500

    fixed a few more links

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Random Errors",
-	url: "https://randomerrors.dev",
+	url: "https://randomerrors.dev/",
 	language: "en",
 	description: "A blog about random errors I encounter while developing software.",
 	author: {
 		name: "John M. Wargo",
 		email: "&#106;&#111;&#104;&#110;&#064;&#106;&#111;&#104;&#110;&#119;&#097;&#114;&#103;&#111;&#046;&#099;&#111;&#109;",
-		url: "https://johnwargo.com"
+		url: "https://johnwargo.com/"
 	}
 }
