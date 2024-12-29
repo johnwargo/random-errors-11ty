f7b56677c104522b75acc42e4af71eb3d558763f
johnwargo <john@johnwargo.com>
2024-12-29 16:41:25 -0500

    replaced eleventyinfo

--- src/_data/eleventyinfo.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const eleventyPackage = require('@11ty/eleventy/package.json')
-
-module.exports = function () {
-  return { generatorStr: `${eleventyPackage.name} v${eleventyPackage.version}` }
-}
---
+++ src/_data/eleventyinfo.mjs
@@ -0,0 +1,6 @@
+import fs from 'fs';
+
+export default function () {
+  const packageJson = JSON.parse(fs.readFileSync('./node_modules/@11ty/eleventy/package.json', 'utf8'));
+  return { generatorStr: `${packageJson.name} v${packageJson.version}` }
+}
